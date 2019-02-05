3f1f91b49b922a4cc24e9faa919afeb10e3ef0c0
tim1016 <inkant@gmail.com>
2019-02-05 16:30:46 -0600

    Added the react to the webpack configuration

--- src/app.js
+++ src/app.js
@@ -5,3 +5,12 @@ import "./styles/styles.scss";
 // import {addition, minus} from "./testImportScript";
 // console.log(addition(2,3)); 
 // console.log(minus(2,3));
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+var customElement = React.createElement("h1", null, "Hello World");
+
+var anotherCustomElement = <h1>Hello world 2</h1>;
+ReactDOM.render(anotherCustomElement, document.getElementById("app"));
