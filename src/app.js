77c99b6d6cf34631c0e1dda4c17ee7f33b7ad165
tim1016 <inkant@gmail.com>
2019-02-05 16:06:52 -0600

    removed script.js from inclusion renamed it to testImportScript.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import 'bootstrap';
-
 import "./styles/styles.scss";
 
 
-import {addition, minus} from "./script";
-
-console.log(addition(2,3)); 
-console.log(minus(2,3));
+// import {addition, minus} from "./testImportScript";
+// console.log(addition(2,3)); 
+// console.log(minus(2,3));
